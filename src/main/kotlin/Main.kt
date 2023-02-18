89021504968cb62a6e8b50a114d16027512d5ce0
joycewanjiku <joycemumbiiwanjiku@gmail.com>
2023-02-18 11:51:02 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main(){
+  subtration(22,11)
+    multiply(64,5)
+    division(88,2)
+    modle(66,33)
+    school("akirachix")
+}
+
+fun subtration(num1:Int,num2:Int){
+    var sub =num1-num2
+    println("subtration of two numbers "+sub)
+}
+fun multiply(mul1:Int,mul2:Int){
+    var mult =mul1*mul2
+    println("multiply of two numbers "+mult)
+}
+fun division(div1:Int,div2:Int){
+    var div =div1/div2
+    println("division of two numbers "+div)
+}
+fun modle(mod1:Int,mod2:Int){
+    var mod =mod1%mod2
+    println("modle of two numbers "+mod)
+}
+fun school(name:String){
+    println("my school name "+name)
+
+}
